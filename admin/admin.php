a9e15791e550909d6cd6c93b28350d5503298b3c
sadoway7 <sadoway@gmail.com>
2025-02-17 00:25:19 -0700

    55

--- admin/admin.php
+++ admin/admin.php
@@ -12,7 +12,7 @@ $categories = cclist_get_categories();
     <a href="<?php echo admin_url('admin.php?page=cclist-admin-new'); ?>" class="page-title-action">Add New Product</a>
     
     
-    <textarea id="import-json" placeholder="Paste JSON data here..." style="width: 80%; height: 200px; display:none;"></textarea>
+    <textarea id="import-json" placeholder="Paste JSON data here..." style="width: 80%; height: 200px;"></textarea>
     <button type="button" class="button button-secondary" id="import-products">Import Products</button>
 
     <div class="cclist-filters">
